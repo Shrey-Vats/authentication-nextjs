500d1544e9fb73a96ba95db40c0ac0dcf73bec69
Shrey vats <techtechnicalshrey@gmail.com>
2025-06-29 02:33:40 +0530

    Inserlize the project

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+
+export default function LoginPage() {
+
+    return (
+        <div>
+            <p>hello world</p>
+        </div>
+    )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+
+export default function SignupPage() {
+
+    return (
+        <div>
+            <p>hello world! Signup </p>
+        </div>
+    )
+}
