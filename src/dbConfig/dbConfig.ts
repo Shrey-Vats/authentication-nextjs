61950467829d63c2eddbe3cf44c444e40227b5d9
Shrey vats <techtechnicalshrey@gmail.com>
2025-06-29 02:45:11 +0530

    setup mongodb connection

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export async function Connection() {
+
+    try {
+        await mongoose.connect(process.env.MONGO_URL!)
+        const connection = mongoose.connection;
+
+        connection.on("connected", ()=>{
+            console.log("MongoDB connected successfully");
+        })
+
+        connection.on("error", (err)=>{
+            console.log("MongoDB connection error:", err);
+            process.exit();
+        })
+
+    } catch (error) {
+        console.log("Error connecting to MongoDB:", error);
+    }
+}
