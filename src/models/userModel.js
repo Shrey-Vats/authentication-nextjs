34be005356e7aad687598fe5688ae3c93a5fc94e
Shrey vats <techtechnicalshrey@gmail.com>
2025-06-29 16:30:52 +0530

    setup userModel

---
+++ src/models/userModel.js
@@ -0,0 +1,36 @@
+import { verify } from 'crypto';
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+})
+
+const User = mongoose.models.users || mongoose.model('User', userSchema);
+
+export default User;
