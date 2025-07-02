e3562f009349879605507cec12ecda7bb9a98e58
Shrey vats <techtechnicalshrey@gmail.com>
2025-07-02 12:46:39 +0530

    create me route

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -5,8 +5,8 @@ export function getDataFromToken(request: NextRequest) {
     try {
         const token:any =request.cookies.get("token")?.value || false
         const decordedToken:any = jwt.verify(token, process.env.JWT_SECRET!);
-        return decordedToken;
-        
+        return decordedToken.id;
+
     } catch (error) {
         throw new Error("Failed to retrieve token from request cookies");
     }
